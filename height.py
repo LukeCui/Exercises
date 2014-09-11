70067f8fd8d7dc596ae0e7b3e11df42f2289295a
LukeCui <luke.cui@gordon.edu>
2014-09-11 14:22:25 -0400

    exercise1
    calculate the height of a free fall object

---
+++ height.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+import math
+h =input("Please type the height(unit:meter)."
+a =9.8m/(s^2)
+t = math.sqrt(2.0*float(h)/a)
+print ("After %s s,the ball hit the ground.(s means second.)")
+
